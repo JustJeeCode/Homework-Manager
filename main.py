b753d312c6b66e44303df65cde4504294f9b299d
Jee <giorpieri@gmail.com>
2021-01-23 22:08:11 +1000

    Coded the Title and starting the code for the underline for the title

--- main.py
+++ main.py
@@ -6,9 +6,25 @@ created to learn how to use Tkinter and have a app to manage homework!
 
 from tkinter import *
 
+# Settings for the GUI
 root = Tk()
+root.title("Homework Manager!")
+root.geometry("800x800")
 
-myButton = Button(root, text="click me", padx=50, pady=50)
-myButton.pack()
+# Title
+appTitle = Label(root, text="Homework Manager!", font="Roboto 30")
+appTitle.pack(pady="20", padx="5")
+
+# Title underline
+line = Canvas(root, width=400, height=3, bd=0,highlightthickness=0)
+line.configure(bg="black")
+line.pack(pady="5", padx="5")
+
+
+
+
+
+
+
 
 root.mainloop()
