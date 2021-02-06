fc5516bbdf07c8fae2f776874c25b4652bb0e16a
Jee <giorpieri@gmail.com>
2021-02-06 21:23:15 +1000

    adding oop and classes to code

---
+++ app.py
@@ -0,0 +1,79 @@
+# Imports
+import tkinter as tk
+
+# Variables
+width = 1920
+height = 1080
+
+root = tk.Tk()
+root.geometry(str(width) + "x" + str(height))
+root.title("Homework Manager!")
+
+
+# Header Class
+class Header(tk.Frame):
+    def __init__(self):
+        # Heading
+        self.homeworkHeading = tk.Label(root, text = "Homework Manager", font = "CaterOne_Font 30")
+        self.homeworkHeading.grid(row = 1, column = 1, padx = 10, pady = 10)
+
+        # Heading Underline
+        self.titleUnderline = tk.Canvas(root, width = width, height = 3, bd= 0, highlightthickness = 0)
+        self.titleUnderline.configure(bg = "black")
+        self.titleUnderline.grid(row = 2, column = 1, pady = 10, padx = 0)
+
+
+# Homework Class
+class Homework(tk.Frame):
+    def __init__(self):
+        # Homework Label
+        self.homeworkLabel = tk.Label(root, text = "Enter Homework To Do:", font = "Roboto 24")
+        self.homeworkLabel.grid(row = 3, column = 1, pady = 50, padx = 0)
+
+        # Homework Entry
+        self.homeworkEntry = tk.Entry(root, width = 50, bd = 4, bg = "white", font="Roboto 12")
+        self.homeworkEntry.grid(row = 4, column = 1, pady = 0, padx = 0)
+
+
+# Date Class
+class Date(tk.Frame):
+    def __init__(self):
+        # Date Label
+        self.dateLabel = tk.Label(root, text = "Enter Date Due:", font = "Roboto 24")
+        self.dateLabel.grid(row = 5, column = 1, pady = 50, padx = 0)
+
+        # Date Entry
+        self.dateEntry = tk.Entry(root, width = 50, bd = 4, bg = "white", font = "Roboto 12")
+        self.dateEntry.grid(row = 6, column = 1, pady = 0, padx = 0)
+
+
+# NewSubmission Class
+class NewSubmission(tk.Frame):
+    def __init__(self):
+        self.submitButton = tk.Button(root, text = "Submit", font = "Roboto 12", height = 2, width = 12, activebackground = "lightgreen", command = print("print"))
+        self.submitButton.grid(row = 7, column = 1, pady = 50, padx = 0)
+
+
+# Homework Todo List Class
+class TodoTitle(tk.Frame):
+    def __init__(self):
+        self.overline = tk.Canvas(root, width = width, height = 3, bd = 0,highlightthickness = 0)
+        self.overline.configure(bg = "black")
+        self.overline.grid(row = 8, column = 1, pady = 10, padx = 0)
+
+        # Homework todo label
+        self.TodoLabel = tk.Label(root, text = "Homework Todo:", font = "Roboto 24")
+        self.TodoLabel.grid(row = 9, column = 1, pady = 10, padx = 0)
+
+
+# Main App
+class MainApplication(tk.Frame):
+    header = Header()
+    homework = Homework()
+    date = Date()
+    newSubmission = NewSubmission()
+    TodoTitle = TodoTitle()
+
+# Packing MainApp
+MainApplication(root).grid(row = 0, column = 0)
+root.mainloop()
--- main.py
+++ main.py
@@ -1,116 +1,13 @@
-"""
+""" 
 Homework Manager App!
 created by JustJee 22/01/2021 
 created to learn how to use Tkinter and have a app to manage homework!
-"""
-
-# GUI resolution
-width = 1920
-height = 1080
-
-# Imports
-from tkinter import *
-from tkinter import ttk
 
 
-# Variables
-global savedHomework
-global savedDate
-global homeworkList
-savedHomework = ""
-savedDate = ""
-homeworkList = []
+import tkinter as tk
 
+class Main(tk.Frame):
+    def __init__(self):
+        pass
 
-# Settings for the GUI
-root = Tk()
-root.title("Homework Manager!")
-root.geometry(str(width) + "x" + str(height))
-
-
-
-class Rows:
-    def __init__(self, homeworkRow, dateRow):
-        self.homeworkRow = homeworkRow
-        self.dateRow = dateRow
-
-    def add_rows(self):
-        self.homeworkRow += 2
-        self.dateRow += 2
-
-rows = Rows(11, 12)
-
-class Entrys:
-    def __init__(self, savedHomework, savedDate, homeworkList):
-        self.savedHomework = savedHomework
-        self.savedDate = savedDate
-        self.homeworkList = homeworkList
-
-    def get_entrys(self):
-        self.savedHomework = homeworkTodo.get()
-        self.savedDate = dateDue.get()
-
-        self.homeworkList.append(savedHomework)
-        self.homeworkList.append(rows.homeworkRow)
-        self.homeworkList.append(savedDate)
-        self.homeworkList.append(rows.dateRow)
-
-        rows.add_rows()
-        self.create_entrys()
-
-    def create_entrys(self):
-        newHomework = Label(root, text=self.savedHomework + " | Date due: " + self.savedDate, font="Roboto 18")
-        newHomework.grid(row=rows.homeworkRow, column=0, pady=10, padx=10)
-        deleteButton = Button(root, text="Delete", font="Roboto 10", height=2, width=8, activebackground="red")
-        deleteButton.grid(row=rows.dateRow, column=0, pady=2, padx=10) 
-
-entrys = Entrys(savedHomework, savedDate, homeworkList)
-
-
-
-
-# Over title line work
-line = Canvas(root, width=width, height=3, bd=0,highlightthickness=0)
-line.configure(bg="black")
-line.grid(row=1, column=0, pady=0, padx=0)
-
-# Title
-appTitle = Label(root, text="Homework Manager!", font="Roboto 30")
-appTitle.grid(row=2, column=0, pady=20, padx=5)
-
-# Title underline
-line = Canvas(root, width=width, height=3, bd=0,highlightthickness=0)
-line.configure(bg="black")
-line.grid(row=3, column=0, pady=2, padx=0)
-
-# Homework text
-homeworkText = Label(root, text="Enter homework to do:", font="Roboto 24")
-homeworkText.grid(row=4,column=0, pady=20, padx=5)
-
-# Homework to do text box
-homeworkTodo = Entry(root, width=50, bd=4, bg="white", font="Roboto 12")
-homeworkTodo.grid(row=5, column=0, pady=20, padx=5)
-
-# Time due text
-timeDueText = Label(root, text="Enter date due:", font="Roboto 24")
-timeDueText.grid(row=6,column=0, pady=20, padx=10)
-
-# Date due text box
-dateDue = Entry(root, width=50, bd=4, bg="white", font="Roboto 12")
-dateDue.grid(row=7, column=0, pady=20, padx=10)
-
-# Submit button 
-submitButton = Button(root, text="Submit", font="Roboto 12", height=2, width=12, activebackground="lightgreen", command=entrys.get_entrys)
-submitButton.grid(row=8, column=0, pady=20, padx=5)
-
-# Homework todo label underline
-line = Canvas(root, width=width, height=3, bd=0,highlightthickness=0)
-line.configure(bg="black")
-line.grid(row=9, column=0, pady=30, padx=0)
-
-# Homework todo label
-homeworkTodoLabel = Label(root, text="Homework to-do:", font="Roboto 24")
-homeworkTodoLabel.grid(row=10, column=0, pady=20, padx=10)
-
-
-root.mainloop()
+main = Main() """
