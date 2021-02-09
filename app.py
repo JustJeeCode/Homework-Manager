847417ccf4bf6c12ebe58a800837ff79c15caf69
Jee <giorpieri@gmail.com>
2021-02-09 20:15:52 +1000

    added background to my app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Imports
 import tkinter as tk
 import tkinter.font
+import os
 
 # Variables
 width = 1920
@@ -10,60 +11,57 @@ root = tk.Tk()
 root.geometry(str(width) + "x" + str(height))
 root.title("Homework Manager!")
 
+background = tk.PhotoImage(file = "images\App_background.png")   
+mainCanvas = tk.Canvas(root, width = width, height = height)
+mainCanvas.pack(fill = "both", expand = True)
+
+mainCanvas.create_image(0, 0, image = background, anchor = "nw")
+
 # Header Class
 class Header(tk.Frame):
     def __init__(self):
         # Heading
-        self.homeworkHeading = tk.Label(root, text = "Homework Manager", font = "FunSized 35 bold")
-        self.homeworkHeading.grid(row = 1, column = 1, padx = 10, pady = 10)
+        self.homeworkHeading = mainCanvas.create_text(960, 50, text = "Homework Manager", font = "oldnewspapertypes 40 bold")
 
         # Heading Underline
-        self.titleUnderline = tk.Canvas(root, width = width, height = 3, bd = 0, highlightthickness = 0)
-        self.titleUnderline.configure(bg = "black")
-        self.titleUnderline.grid(row = 2, column = 1, pady = 10, padx = 0)
+        #line = canvas.create_line(x0, y0, x1, y1, ..., xn, yn, options)
+        self.titleUnderline = mainCanvas.create_line(960, 70, 1920, 3)
 
 
 # Homework Class
 class Homework(tk.Frame):
     def __init__(self):
         # Homework Label
-        self.homeworkLabel = tk.Label(root, text = "Enter Homework To Do:", font = "Roboto 24")
-        self.homeworkLabel.grid(row = 3, column = 1, pady = 50, padx = 0)
+        self.homeworkLabel = mainCanvas.create_text(960, 180, text = "Enter Homework To Do:", font = "Linowrite 24")
 
         # Homework Entry
-        self.homeworkEntry = tk.Entry(root, width = 50, bd = 4, bg = "white", font="Roboto 12")
-        self.homeworkEntry.grid(row = 4, column = 1, pady = 0, padx = 0)
+        self.homeworkEntry = mainCanvas.create_window(960, 200)
 
 
 # Date Class
 class Date(tk.Frame):
     def __init__(self):
         # Date Label
-        self.dateLabel = tk.Label(root, text = "Enter Date Due:", font = "Roboto 24")
-        self.dateLabel.grid(row = 5, column = 1, pady = 50, padx = 0)
+        self.dateLabel = mainCanvas.create_text(960, 250, text = "Enter Date Due:", font = "Linowrite 24")
 
         # Date Entry
-        self.dateEntry = tk.Entry(root, width = 50, bd = 4, bg = "white", font = "Roboto 12")
-        self.dateEntry.grid(row = 6, column = 1, pady = 0, padx = 0)
+        self.dateEntry = mainCanvas.create_window(960, 270)
 
 
 # NewSubmission Class
 class NewSubmission(tk.Frame):
     def __init__(self):
-        self.submitButton = tk.Button(root, text = "Submit", font = "Roboto 12", height = 2, width = 12, activebackground = "lightgreen", command = print("print"))
-        self.submitButton.grid(row = 7, column = 1, pady = 50, padx = 0)
-
+        self.submitButton = tk.Button(root, text = "Submit", font = "Linowrite 16", height = 1, width = 12, activebackground = "lightgreen", command = print("print"))
+        self.submitButtonWindow = mainCanvas.create_window(880, 300, anchor = "nw", window = self.submitButton)
 
 # Homework Todo List Class
 class TodoTitle(tk.Frame):
     def __init__(self):
         self.overline = tk.Canvas(root, width = width, height = 3, bd = 0,highlightthickness = 0)
         self.overline.configure(bg = "black")
-        self.overline.grid(row = 8, column = 1, pady = 10, padx = 0)
 
         # Homework todo label
-        self.TodoLabel = tk.Label(root, text = "Homework Todo:", font = "Roboto 24")
-        self.TodoLabel.grid(row = 9, column = 1, pady = 10, padx = 0)
+        self.TodoLabel = mainCanvas.create_text(960, 380, text = "Homework Todo:", font = "Linowrite 24")
 
 
 # Main App
@@ -72,9 +70,9 @@ class MainApplication(tk.Frame):
     homework = Homework()
     date = Date()
     newSubmission = NewSubmission()
-    TodoTitle = TodoTitle()
+    todoTitle = TodoTitle()
 
 
 # Packing MainApp
-MainApplication(root).grid(row = 0, column = 0)
+MainApplication(root).pack(side="top", fill="both", expand=True)
 root.mainloop()
